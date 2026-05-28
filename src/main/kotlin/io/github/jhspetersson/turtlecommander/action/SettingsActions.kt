4bfd4d95b72d5a7ff1a5d09d173d87d22a8719b8
jhspetersson <jhspetersson@gmail.com>
2026-05-29 00:55:04 +0200

    fix deprecations

--- src/main/kotlin/io/github/jhspetersson/turtlecommander/action/SettingsActions.kt
+++ src/main/kotlin/io/github/jhspetersson/turtlecommander/action/SettingsActions.kt
@@ -1,7 +1,7 @@
 package io.github.jhspetersson.turtlecommander.action
 
 import com.intellij.ide.BrowserUtil
-import com.intellij.ide.plugins.PluginManager
+import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
@@ -167,7 +167,7 @@ private class AboutDialog(project: com.intellij.openapi.project.Project?) : Dial
     }
 
     override fun createCenterPanel(): JComponent {
-        val plugin = PluginManager.getInstance().findEnabledPlugin(PluginId.getId("io.github.jhspetersson.turtlecommander"))
+        val plugin = PluginManagerCore.getPlugin(PluginId.getId("io.github.jhspetersson.turtlecommander"))
         val version = plugin?.version ?: "unknown"
         val vendor = plugin?.vendorEmail ?: ""
 
